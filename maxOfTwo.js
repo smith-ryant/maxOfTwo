1527da895a1334d164ae5f139caa4a973920f614
smith-ryant <ryant.smith@outlook.com>
2024-01-16 20:11:04 -0700

    Update to maxOfTwo

--- maxOfTwo.js
+++ maxOfTwo.js
@@ -2,9 +2,12 @@ let x = 3
 let y = 3
 
 // Write your code below this line.
-if (x > y)
+if (x > y){
     console.log(x);
-else if (x < y)
-    console.log(y);
-else 
-    console.log("Both values are equal");
+} else if (x < y) {
+    console.log(y)
+} else {
+    console.log("They are equal numbers.")
+}
+
+
